6b1ad6e01e8a22dd140698ded3df4113bd0d4147
Paul Welby <pwelby@gmail.com>
2023-06-28 11:56:46 -0700

    updating protected files

--- server/config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  HOST: "localhost",
-  PORT: 27017,
-  DB: "node-mongo-signup-verification-api"
-};
-
-// DB_CONN_STRING: 'mongodb+srv://pwelby:AaBbCcXxYyZz12345@cluster0.k82k8qn.mongodb.net/node-mongo-signup-verification-api?retryWrites=true&w=majority'
