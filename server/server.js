c794f08d07b73e2a08c2753754c0614698567718
Paul Welby <pwelby@gmail.com>
2023-06-30 12:25:52 -0700

    changes for deploy to Netlify

--- server/server.js
+++ server/server.js
@@ -44,5 +44,5 @@ app.use(errorHandler);
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
-    console.log("Connected to DB:", DBName + " " + 'via Compass on Cluster0')
+    console.log('Connected to DB:', DBName + " " + 'via Compass on Cluster0')
 });
